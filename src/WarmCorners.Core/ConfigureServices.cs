9a5015a56c2e12491cc37dfa5f9cb0a7aee2c655
Federico Antuña <federicoantuna@hotmail.com>
2023-06-25 10:56:33 -0300

    feat: Added capability to process command triggers

---
+++ src/WarmCorners.Core/ConfigureServices.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WarmCorners.Core.Services;
+using WarmCorners.Core.Services.Abstractions;
+using WarmCorners.Core.Wrappers;
+
+namespace WarmCorners.Core;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddCoreServices(this IServiceCollection services)
+    {
+        services.AddSingleton<ICommandTriggerService, CommandTriggerService>();
+
+        services.AddSingleton<IProcessWrapper, ProcessWrapper>();
+
+        return services;
+    }
+}
---
+++ tests/WarmCorners.Core.Tests.Unit/ServiceProviderBuilder.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Serilog;
+using Serilog.Sinks.InMemory;
+using WarmCorners.Core.Services.Abstractions;
+using WarmCorners.Core.Wrappers;
+
+namespace WarmCorners.Core.Tests.Unit;
+
+public class ServiceProviderBuilder
+{
+    private readonly IServiceCollection _services = new ServiceCollection();
+
+    public ServiceProviderBuilder()
+    {
+        this._services.AddCoreServices();
+
+        this.ReplaceWrappersWithMocks();
+
+        this.AddPresentationServiceMocks();
+
+        this.SetupInMemoryLogger();
+    }
+
+    public IServiceProvider Build() => this._services.BuildServiceProvider();
+
+    private void ReplaceWrappersWithMocks()
+    {
+        var processWrapper = this._services.Single(sd => sd.ServiceType == typeof(IProcessWrapper));
+        this._services.Remove(processWrapper);
+        var processWrapperMock = new Mock<IProcessWrapper>();
+        this._services.AddSingleton(_ => processWrapperMock.Object);
+    }
+
+    private void AddPresentationServiceMocks()
+    {
+        var screenServiceMock = new Mock<IScreenService>();
+        this._services.AddSingleton(_ => screenServiceMock.Object);
+    }
+
+    private void SetupInMemoryLogger() =>
+        this._services.AddLogging(builder =>
+        {
+            builder.ClearProviders();
+
+            var logger = new LoggerConfiguration()
+                .WriteTo.InMemory()
+                .MinimumLevel.Verbose()
+                .CreateLogger();
+            builder.AddSerilog(logger);
+        });
+}
