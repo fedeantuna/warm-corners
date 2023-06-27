668c99939f1b0e7f9a0b60029e61f4b8b4d7954a
Federico Antuña <federicoantuna@hotmail.com>
2023-06-26 23:11:02 -0300

    refactor: Removed unused property from ProcessWrapper

--- src/WarmCorners.Core/Wrappers/ProcessWrapper.cs
+++ src/WarmCorners.Core/Wrappers/ProcessWrapper.cs
@@ -4,7 +4,6 @@ namespace WarmCorners.Core.Wrappers;
 
 public interface IProcessWrapper
 {
-    ProcessStartInfo StartInfo { get; }
     void SetStartInfo(ProcessStartInfo startInfo);
     bool Start();
 }
@@ -12,8 +11,6 @@ public interface IProcessWrapper
 public class ProcessWrapper : IProcessWrapper
 {
     private readonly Process _process = new();
-
-    public ProcessStartInfo StartInfo => this._process.StartInfo;
 
     public void SetStartInfo(ProcessStartInfo startInfo)
         => this._process.StartInfo = startInfo;
